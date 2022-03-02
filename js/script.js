9428bf806b94ed02cc65d1b746cae2bc95f606f9
wishtree-hkshirsagar <hrishikesh.kshirsagar@wishtreetech.com>
2022-03-02 11:48:08 +0530

    Completed responsive behavior of home page

--- js/script.js
+++ js/script.js
@@ -22,3 +22,23 @@ $(function() {
         navText: ["<img src='images/home-page/down-arrow.svg'>", "<img src='images/home-page/down-arrow-1.svg'>"]
     });
 });
+
+/*=====================================
+            Mobile Navigation
+=======================================*/
+
+const navBtn = $("#nav-btn");
+const navbar = $("#mobile-navigation");
+const navClose = $("#nav-close");
+
+// show nav
+navBtn.on("click", () => {
+    console.log('inside navbtn');
+    navbar.addClass("showNav");
+});
+
+// close nav
+navClose.on("click", () => {
+    console.log('inside closebtn');
+    navbar.removeClass("showNav");
+});
